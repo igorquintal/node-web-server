33d041aaa8171d5be86a5825893e8cae886f1e39
Igor Quintal Mendes <igorquintalmendes@gmail.com>
2017-09-02 12:24:29 -0300

    Deploying a new Projects page

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle : 'Projects Page',
+    projectsMessage: 'These are my projects!'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage : 'Unable to fulfill request!'
